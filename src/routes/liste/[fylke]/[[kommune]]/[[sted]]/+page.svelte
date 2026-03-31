ec50bc3d2b879b4c411caf671816d2a9e6c85933
Gard Ruud <gruud@tutanota.com>
2026-03-31 15:06:52 +0200

    [CHANGE] added ugly code to get title in Liste

--- src/routes/liste/[fylke]/[[kommune]]/[[sted]]/+page.svelte
+++ src/routes/liste/[fylke]/[[kommune]]/[[sted]]/+page.svelte
@@ -1,9 +1,18 @@
 <script lang="js">
-  import { Button } from "$lib/components/ui/button/index.js";
-	import Breadcrumb from "@/components/internal/Breadcrumb.svelte";
-	import BarList from "@/components/internal/BarList.svelte";
-	import NavGroup from "@/components/internal/NavGroup.svelte";
-  export let data;
+  import { page } from '$app/stores';
+  import BarList from "@/components/internal/BarList.svelte";
+  import Breadcrumb from "@/components/internal/Breadcrumb.svelte";
+  import NavGroup from "@/components/internal/NavGroup.svelte";
+  import { H2 } from '@/styles/styles.js';
+  let { data } = $props();
+
+  const title = $derived(
+		$page.params.sted
+			? data.list.breadcrumb[2].name
+			: $page.params.kommune
+				? data.list.breadcrumb[1].name
+				: data.list.breadcrumb[0].name
+	);
 </script>
 
 <article class="m-auto max-w-xl">
@@ -13,5 +22,6 @@
     <div class="mb-2">
       <NavGroup links={data.list.links} />
     </div>
+  <h2 class={H2}>Ølpriser – {title}</h2>
   <BarList bars={data.list.bars}/>
 </article>
