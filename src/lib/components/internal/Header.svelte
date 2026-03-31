94afb91e9cc5077f1ae6f63f37424b4eaa0fba6d
Gard Ruud <gruud@tutanota.com>
2026-03-31 14:19:46 +0200

    [CHANGE] added padding to header

--- src/lib/components/internal/Header.svelte
+++ src/lib/components/internal/Header.svelte
@@ -2,7 +2,7 @@
     import Search from '@/components/internal/Search.svelte';
 </script>
 
-<header class="py-2 mb-4 font-semibold bg-amber-200">
+<header class="py-2 mb-4 font-semibold bg-amber-200 max-sm:px-4">
     <nav class="flex justify-between m-auto max-w-xl">
         <div class="flex gap-2 items-baseline pr-4">
             <a
