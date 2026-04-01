7314b5c9677a21501a2d653d7c0f25d2f068e6d7
Gard Ruud <gruud@tutanota.com>
2026-04-01 13:17:36 +0200

    [CHORE] removed unused styles and un-commented Link style

--- src/lib/styles/styles.js
+++ src/lib/styles/styles.js
@@ -1,21 +1,5 @@
-export const btnBase = "px-2 py-1 rounded font-semibold text-sm transition "
-export const BtnPrimary = btnBase + "bg-violet-200 hover:bg-violet-300"
-export const BtnSecondary = btnBase + "bg-emerald-200 hover:bg-emerald-300"
-export const BtnWarning = btnBase + "bg-red-200 hover:bg-red-300"
 
-// export const url = "text-blue-600 hover:text-blue-700 underline underline-offset-4 hover:no-underline"
-export const PopupContainer = "fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-amber-200  rounded-lg z-50 p-4"
-
-export const Input = "w-full bg-amber-50  rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-violet-200 focus:ring-2 focus:ring-violet-200 focus:outline-none sm:text-sm"
-export const Curtain = "block w-full rounded-md border border-gray-300 bg-amber-50  px-3 py-2 pr-8 shadow-sm focus:border-violet-200 focus:outline-none focus:ring-2 focus:ring-violet-200 sm:text-sm"
-export const BtnLink = btnBase + "md:hover:bg-violet-400 active:bg-violet-400 bg-violet-300 outline outline-2 outline-slate-700"
-
-export const TableWrap = "overflow-x-auto rounded-xl shadow ring-1 ring-black/5"
-export const Table = "min-w-full table-auto border-collapse"
-export const TableHead = "bg-slate-50"
-export const TableTH = "px-4 py-3 text-left font-semibold"
-export const TableBody = "[&_td]:px-4 [&_td]:py-3 [&_td]:align-middle"
-export const TableRow = "odd:bg-white even:bg-slate-50 hover:bg-slate-100 border-b border-slate-200"
+export const Link = "hover:text-blue-700 underline underline-offset-4 hover:no-underline"
 
 export const H2 = "text-2xl font-semibold tracking-tight mb-3"
 export const H3 = "text-xl font-medium tracking-tight mb-2"
