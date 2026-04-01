152937090d9d0bbd07cb04f9c2342baa59bc81c4
Gard Ruud <gruud@tutanota.com>
2026-04-01 13:15:46 +0200

    [CHANGE] updated copy

--- src/routes/om-oss/+page.svelte
+++ src/routes/om-oss/+page.svelte
@@ -1,100 +1,76 @@
 <script lang="ts">
-	import MailToPilsguiden from "@/components/internal/MailToPilsguiden.svelte";
+	import MailToPilsguiden from '@/components/internal/MailToPilsguiden.svelte';
+	import { resolve } from '$app/paths';
+	import { H2, H3, Link } from '@/styles/styles.js';
 
-  export let data;
-  const { about } = data;
+	let { data } = $props();
 </script>
 
 <article class="m-auto max-w-xl">
-    <section>
-        <h2 class="text-3xl font-extrabold pb-4">Om Pilsguiden</h2>
-        <div class="flex flex-col gap-1">
-            <p class="max-w-prose">
-                Pilsguiden ble etablert i september 2023 med et mål om å være den mest komplette
-                og oppdaterte siden for ølpriser i Norge. Akkurat nå er det {about.total_bars}serveringssteder
-                på siden fordelt over alle fylkene med Svalbard. 
-            </p>
-            <p class="max-w-prose">
-                Ølprisen kan variere voldsomt i Norge.
-                Blant serveringsstedene på siden, koster den billigste ølen {about.min_price}kr
-                og den dyreste {about.max_price}kr, noe som utgjør en forskjell på {about.price_dif}kr.* 
-                Pilsguiden ble til for å lose deg i retning et serveringssted som passer din økonomi og for å skape litt
-                forutsigbarhet når du ber om en pils.
-            </p>
-            <p class="max-w-prose">
-                * Alle oppgitte priser er NOK per halvliter øl.
-            </p>
-        </div>
-    </section>
-    <section>
-        <h3 class="text-xl font-bold my-2">Slik samler vi inn priser</h3>
-        <div class="max-w-prose flex flex-col gap-2">
-            <p>
-                Ølprisene på Pilsguiden.no hentes inn ved nettdugnad. Brukere over hele landet
-                kan kontakte oss på <MailToPilsguiden /> for å melde inn priser. Da trenger vi navnet på serveringsstedet, prisen på et glass øl og størrelsen på glasset.
-                Prisen blir automatisk konvertert til halvliter-pris når vi legger det inn i systemet. Vi vil helst at du
-                også inkluderer hvilket bryggeri baren fører, da det er mulig å
-                filtrere listene våre slik at man kan finne steder som fører sitt favorittbryggeri.
-            </p>
-            <p>
-                Skal du sende tidsbestemte priser, også kalt «happy hour» på folkemunne, er det viktig
-                at vi får tidene og dagene det gjelder, samt prisen på ølen ellers. Listene oppdaterer
-                seg i sanntid, så prisene som vises i lista er prisene som gjelder akkurat nå.
-                Ser du en klokke ⏰ i lista kan du trykke på det aktuelle serveringsstedet og se hvor lenge prisen gjelder. 
-            </p>
-            <p>
-                Finner du en pris i lista som fortsatt er riktig? Da hjelper du alle ved å bruke «bekreft pris»-funksjonen.
-                Da oppdateres datoen og datakvaliteten blir bedre. 
-            </p>
-            <p>
-                I tillegg til prisene som blir sendt inn, gjør vi også en jobb med å sjekke menyer og bestillingsløsninger
-                som finnes på nett, og henter priser derfra om vi ser på datakvaliteten som pålitelig.
-                Men aller helst ønsker vi oss innsendte priser. 
-            </p>
-        </div>
-    </section>
-    <section>
-        <h3 class="text-xl font-bold my-2">Disse prisene finner du på Pilsguiden</h3>
-        <div class="flex flex-col gap-2">
-            <p class="max-w-prose">
-                For at lista skal fungere best mulig, har vi valgt å sette opp noen krav til prisene vi samler inn,
-                slik at sammenligningsgrunnlaget skal være best mulig. Når det gjelder serveringssteder tar vi inn alt
-                fra barer, restauranter, hoteller, kinoer, flyplasser osv. så lenge stedet ikke regnes som privat.
-            </p>
-            <p class="max-w-prose">
-                I tillegg har vi disse kravene: 
-            </p>
-            <h4 class="font-semibold ml-2">• Prisen er for pils på tapp</h4>
-            <p class="max-w-prose">
-                Vi er strenge på dette for at sammenligningen skal være mest mulig epler og epler. Det er sikkert mange gode
-                tilbud på bokser og flasker, men det er noe man like gjerne kan konsumere hjemme. 
-            </p>
-            <h4 class="font-semibold ml-2">• Typisk det man får når man sier «en øl/pils, takk!»</h4>
-            <p class="max-w-prose">
-                Pilsen favner bredt og det er ukontroversielt å si at den er en standard. Om noen vil lage juicyipaguiden, bayerguiden osv.,
-                ønsker vi dem velkommen til det, men her er pils hellig.
-            </p>
-            <h4 class="font-semibold ml-2">• Ingen kvantumsrabatter</h4>
-            <p class="max-w-prose">
-                Noen utesteder gir lavere pris dersom du kjøper en mugge eller flere enheter. Vi kunne selvfølgelig omregnet dette til
-                halvliterspris, men vi er opptatt av at Pilsguiden skal fungere enten du vil ta en øl alene eller med venner.
-                Vi har forståelse for at serveringsstedene ønsker å holde kunder hos seg lenger, men vi er ikke promoteringsplattform
-                og ønsker ikke å oppfordre til økt konsum. 
-            </p>
-            <h4 class="font-semibold ml-2">• En pris alle kan få</h4>
-            <p class="max-w-prose">
-                Det finnes rabatter man kan få dersom man for eksempel er stamkunde eller student.
-                Vi er opptatt av at siden skal kunne brukes av flest mulig, og per i dag har vi ikke planer om
-                å legge inn priser som ikke alle kan få.
-            </p>
-        </div>
-    </section>
-    <section>
-        <h3 class="text-xl font-bold my-2">Hvem står bak Pilsguiden?</h3>
-        <p class="max-w-prose">
-            Utover alle pilsguider (eller «pilsfugler» om du fant oss via TV2) som sender oss priser,
-            er vi to som står bak nettsiden: <a href="https://www.linkedin.com/in/garu/" class="font-semibold underline">Gard Ruud</a> og
-            <a href="https://www.linkedin.com/in/tonynorgaard/" class="font-semibold underline">Tony Norgaard</a>.
-        </p>
-    </section>
+	<section>
+		<h2 class={H2}>Om Pilsguiden</h2>
+		<div class="flex flex-col gap-1">
+			<p class="max-w-prose">
+				Pilsguiden ble etablert i september 2023 med et mål om å være den mest komplette og
+				oppdaterte siden for ølpriser i Norge. Akkurat nå har vi priser for {data.about.total_bars} serveringssteder
+				på siden fordelt over hele Norge.
+			</p>
+			<p class="max-w-prose">
+				Ølprisen kan variere voldsomt i Norge. Blant serveringsstedene på siden, koster den
+				billigste ølen {data.about.min_price}kr og den dyreste {data.about.max_price}kr, noe som
+				utgjør en forskjell på {data.about.price_dif}kr.* Pilsguiden ble til for å lose deg i
+				retning et serveringssted som passer din økonomi og for å skape litt forutsigbarhet når du
+				ber om en pils.
+			</p>
+			<p class="max-w-prose">* Alle oppgitte priser er NOK per halvliter øl.</p>
+		</div>
+	</section>
+	<section>
+		<h3 class={H3}>En pils, takk!</h3>
+		<div class="flex flex-col gap-1">
+			<p class="max-w-prose">
+				Prisene på Pilsguiden er samlet om et enkelt spørsmål: hva betaler du når du ber om en pils.
+				Fordi vi hater shrinkflation og elsker halvliteren, er prisgrunnlaget på hele nettsiden i
+				halvliterspris.
+			</p>
+			<p class="max-w-prose">
+				Vi ser det også er et ønske om innsikt i resten av menyen til barer. Dette aspirerer vi ikke
+				til å vise på Pilsguiden, men vi oppfordrer andre til å lage tjenester som tar et mer
+				helhetlig blikk på serveringssteder. Vi tilbyr i dag en API-tjeneste du kan lese mer om <a
+					href={resolve('/om-oss/api')}
+					class={Link}>her</a
+				>.
+			</p>
+			<p class="max-w-prose">
+				Vi er også opptatt av at det er en pris alle kan få. Vi samler derfor ikke inn student-
+				eller vennepriser.
+			</p>
+		</div>
+	</section>
+	<section>
+		<h3 class="my-2 text-xl font-bold">Datakilde og -kvalitet</h3>
+		<div class="flex max-w-prose flex-col gap-2">
+			<p>
+				Prisdata er samlet i dugnadsånd. Vi foretar i dag både maskinell og menneskelig kontroll av
+				prisen, men vi er avhengige av at brukerne bidrar til å rapportere feil. Først og fremst
+				gjennom å bekrefte prisene som stemmer, og sende inn oppdatert pris der det ikke stemmer.
+				For mer kompliserte feil, setter vi pris på at vi kontakter på <MailToPilsguiden />.
+			</p>
+		</div>
+	</section>
+	<section>
+		<h3 class="my-2 text-xl font-bold">Uavhengighetserklæring</h3>
+		<div class="flex flex-col gap-2">
+			<p class="max-w-prose">
+				Pilsguiden og API-et i bakkant er laget og driftet av <a
+					href="https://www.linkedin.com/in/tonynorgaard/"
+					class="font-semibold underline">Tony Norgaard</a
+				>
+				og
+				<a href="https://www.linkedin.com/in/garu/" class="font-semibold underline">Gard Ruud</a>.
+				Vi er ikke affiliert med serveringsbransjen og forplikter oss til å forbli uavhengige så
+				lenge vi forvalter Pilsguiden og Pilsguiden-API som en del av Pilz-gruppens produktpakke.
+			</p>
+		</div>
+	</section>
 </article>
